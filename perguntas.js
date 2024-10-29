6ea19ee8eacec5270a0daf6a9cc55113b3109a60
nicollycb <140823269+nicollycb@users.noreply.github.com>
2024-10-29 08:46:57 -0300

    29.10

---
+++ perguntas.js
@@ -0,0 +1,56 @@
+criaCartao (
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Biologia',
+    'De acordo com o fixismo, é correto afirmar que',
+    'As espécie vivas atualmente são idênticas as do passado'
+)
+criaCartao(
+    'Matemática 1',
+    'Qual é o tempo máximo de contrato para um estagiário em uma empresa?',
+    'Dois anos'
+)
+criaCartao(
+    'Ed financeira',
+    'O que pode contribuir para um superávit comercial de um país?',
+    'Aumento na exportações de produtos manufaturados'
+)
+criaCartao(
+    'Geografia',
+    'Quais foram os principais fatores responsáveis pela formação da população brasileira ao longo dos séculos?',
+    'O processo de colonização portuguesa e o tráfico transatlântico de escravos africanos'
+)
+criaCartao(
+    'Biologia I',
+    'Quais são as 4 principais aplicações da biotecnologia?',
+    'Medicina, indústria, alimentação e meio ambiente.'
+)
+criaCartao(
+    'Química',
+    'Alcinos são hidrocarbonetos:',
+    'Alifáticos insaturados com tripla ligação'
+)
+criaCartao(
+    'Matemática',
+    'Como é chamado o maior lado do triângulo retângulo?',
+    'Hipotenusa'
+)
+criaCartao(
+    'Sociologia',
+    'Émile Durkheim, autor clásico a sociologia, considera o trabalho um elemento fundamental da sociedade capitalista, porque?',
+    'Por meio da divisão do trabalho social estabelece relações de interdependência entre os indivíduos, o que fortalece a coesão social.'
+
+)
