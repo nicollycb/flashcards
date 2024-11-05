fdc55b0756ed8d48d4edf1d198af8548b6bded87
nicollycb <140823269+nicollycb@users.noreply.github.com>
2024-11-05 08:18:27 -0300

    .

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ function criaCartao(categoria, pergunta, resposta){
     cartao.className = 'cartao'
     cartao.innerHTML = `
     <div class="cartao__conteudo"> 
-                    <h3>$(categoria)</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
-                        <p>$(pergunta)</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
-                        <p>$(resposta)</p>
+                        <p>${resposta}</p>
                     </div>
                 </div>
     `
+    let container = document.getElementById('container')
+    container.appendChild(cartao)
 }
